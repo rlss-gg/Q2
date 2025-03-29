7cec5c0ca1cb36e6a5a8a99414b8d027ff5d2726
leydel <me@itsf.in>
2025-03-29 16:31:34 +1000

    Add functions for interacting with match and create interface for durable entity abstraction

--- Q2.Domain/Match.fs
+++ Q2.Domain/Match.fs
@@ -4,18 +4,30 @@ open System
 
 type MatchTeam = {
     Players: string list
+    Score: int option
 }
 
 module MatchTeam =
     let create players =
         {
             Players = players
+            Score = None
         }
+
+    let setScore score team =
+        { team with Score = score }
+
+[<RequireQualifiedAccess>]
+type MatchStatus =
+    | InProgress
+    | Completed of completedAt: DateTime
+    | Abandoned of abandonedAt: DateTime
 
 type Match = {
     Id: string
     Teams: MatchTeam * MatchTeam
     CreatedAt: DateTime
+    Status: MatchStatus
 }
 
 module Match =
@@ -24,4 +36,17 @@ module Match =
             Id = id
             Teams = team1, team2
             CreatedAt = currentTime
+            Status = MatchStatus.InProgress
         }
+
+    let report score1 score2 currentTime match' =
+        let teams =
+            fst match'.Teams |> MatchTeam.setScore (Some score1),
+            snd match'.Teams |> MatchTeam.setScore (Some score2)
+
+        { match' with
+            Teams = teams
+            Status = MatchStatus.Completed currentTime }
+
+    let abandon currentTime match' =
+        { match' with Status = MatchStatus.Abandoned currentTime }
