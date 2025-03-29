8253188e8a7b4a10308ff31d2a3d699c196ef2a1
leydel <me@itsf.in>
2025-03-29 23:09:29 +1000

    Create interaction endpoint and stub for handling specific commands

---
+++ Q2.Application/Services/InteractionService.fs
@@ -0,0 +1,11 @@
+﻿module Q2.Application.InteractionService
+
+open FSharp.Discord.Types
+
+let handle (interaction: Interaction) = task {
+    // TODO: Match the interaction to command here and call use cases
+    // TODO: Partially apply the use cases for testability
+    // TODO: Should this be enqueued so the function can reutrn 202 immediately?
+
+    return ()
+}
---
+++ Q2.Presentation/Controllers/InteractionController.fs
@@ -0,0 +1,58 @@
+﻿namespace Q2.Presentation
+
+open FSharp.Discord.Types
+open FSharp.Discord.Types.Serialization
+open FSharp.Discord.Webhook
+open Microsoft.Azure.Functions.Worker
+open Microsoft.Azure.Functions.Worker.Http
+open Microsoft.Extensions.Configuration
+open Q2.Application
+open System.Net
+open Thoth.Json.Net
+
+type InteractionController (configuration: IConfiguration) =
+    let publicKey = configuration.GetValue<string> "DiscordPublicKey"
+
+    [<Function "PostInteraction">]
+    member _.Post (
+        [<HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "interactions")>] req: HttpRequestData
+    ) = task {
+        let! json = req.ReadAsStringAsync()
+
+        let signature =
+            req.Headers.TryGetValues "x-signature-ed25519"
+            |> fun (success, v) -> match success with | true -> Seq.tryHead v | false -> None
+
+        let timestamp =
+            req.Headers.TryGetValues "x-signature-timestamp"
+            |> fun (success, v) -> match success with | true -> Seq.tryHead v | false -> None
+
+        // Ensure headers are present
+        match signature, timestamp with
+        | None, None | None, Some _ | Some _, None -> return req.CreateResponse HttpStatusCode.Unauthorized
+        | Some signature, Some timestamp ->
+        
+        // Validate signature
+        match Ed25519.verify timestamp json signature publicKey with
+        | false -> return req.CreateResponse HttpStatusCode.Unauthorized
+        | _ ->
+            
+        // Read interaction from body
+        match Decode.fromString Interaction.decoder json with
+        | Error _ -> return req.CreateResponse HttpStatusCode.BadRequest
+        | Ok interaction ->
+
+        // Handle interaction
+        match interaction with
+        | { Type = InteractionType.PING } ->
+            let callback = { Type = InteractionCallbackType.PONG; Data = None }
+            let body = Encode.toString 0 (InteractionResponse.encoder callback)
+            
+            let res = req.CreateResponse HttpStatusCode.OK
+            do! res.WriteStringAsync body
+            return res
+
+        | interaction ->
+            do! InteractionService.handle interaction
+            return req.CreateResponse HttpStatusCode.Accepted
+    }
