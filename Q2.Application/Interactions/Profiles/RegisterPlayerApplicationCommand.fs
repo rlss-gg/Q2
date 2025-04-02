192fafd6c8194961fdf439888a26cdcaf7147dac
leydel <me@itsf.in>
2025-04-02 17:00:55 +1000

    Separate interaction matching to be handled separately for each command

---
+++ Q2.Application/Interactions/Profiles/RegisterPlayerApplicationCommand.fs
@@ -0,0 +1,57 @@
+﻿module Q2.Application.RegisterPlayerApplicationCommand
+
+open FSharp.Discord.Commands
+open FSharp.Discord.Types
+open Q2.Domain
+
+[<RequireQualifiedAccess>]
+type Action =
+    | ServerAutocomplete of string
+    | RankAutocomplete   of string
+    | ModeAutocomplete   of string
+    | RunCommand         of RegisterPlayerUseCase
+
+// TODO: Should these contain use case payloads?
+
+let (|Validate|_|) (interaction: Interaction) =
+    // TODO: Do we really want mode and specific region? How could this be improved?
+    // TODO: Preferably autocomplete could just be replaced with choices
+
+    match interaction with
+    | ApplicationCommandAutocomplete "register" { Options = Some (
+        String.Autocomplete "server" server
+    ) } ->
+        Some (Action.ServerAutocomplete server)
+
+    | ApplicationCommandAutocomplete "register" { Options = Some (
+        String.Autocomplete "rank" rank
+    ) } ->
+        Some (Action.RankAutocomplete rank)
+
+    | ApplicationCommandAutocomplete "register" { Options = Some (
+        String.Autocomplete "mode" mode
+    ) } ->
+        Some (Action.ModeAutocomplete mode)
+
+    | ApplicationCommand "register" { Options = Some (
+        String.Required "username" username &
+        String.Required "server" server &
+        String.Required "rank" rank &
+        String.Required "mode" mode
+    ) } ->
+        let userId =
+            match interaction.Author with
+            | InteractionAuthor.User u -> u.Id
+            | InteractionAuthor.GuildMember m -> m.User |> Option.map _.Id |> Option.get
+
+        Some (Action.RunCommand {
+            Id = userId
+            Username = username
+            PrimaryServer = GameServer.fromString server |> Option.get
+            Rank = GameRank.fromString rank |> Option.get
+            RankMode = GameMode.fromString mode |> Option.get
+        })
+
+    | _ -> None
+
+// TODO: Function to run the given actions (?)
--- Q2.Application/Services/InteractionService.fs
+++ Q2.Application/Services/InteractionService.fs
@@ -1,67 +1,12 @@
 ﻿module Q2.Application.InteractionService
 
-open FSharp.Discord.Commands
-open FSharp.Discord.Types
-open Q2.Domain
+// TODO: Figure out a more appropriate name for this module
 
-let handle env (interaction: Interaction) = task {
+let handle env interaction = task {
     match interaction with
-    | ApplicationCommandAutocomplete "register" { Options = Some (
-        String.Autocomplete "server" server
-    ) } ->
-        return () // TODO: Implement
+    | RegisterPlayerApplicationCommand.Validate action -> return ()
+    | _ -> return ()
 
-    | ApplicationCommandAutocomplete "register" { Options = Some (
-        String.Autocomplete "rank" rank
-    ) } ->
-        return () // TODO: Implement
-
-    | ApplicationCommandAutocomplete "register" { Options = Some (
-        String.Autocomplete "mode" mode
-    ) } ->
-        return () // TODO: Implement
-
-    | ApplicationCommand "register" { Options = Some (
-        String.Required "username" username &
-        String.Required "server" server &
-        String.Required "rank" rank &
-        String.Required "mode" mode
-    ) } ->
-        let userId =
-            match interaction.Author with
-            | InteractionAuthor.User u -> u.Id
-            | InteractionAuthor.GuildMember m -> m.User |> Option.map _.Id |> Option.get
-
-        let server = GameServer.fromString server
-        let rank = GameRank.fromString rank
-        let mode = GameMode.fromString mode
-
-        // TODO: These can probably be changed to choices which can then match perfectly (how to handle rank then?)
-        // TODO: If going the choices route, can probably make special options active patterns for extracting to enum
-        // TODO: Do we really want mode and specific region? How could this be improved?
-
-        match server, rank, mode with
-        | Some server, Some rank, Some mode ->
-            let! res = RegisterPlayerUseCase.run env {
-                Id = userId
-                Username = username
-                PrimaryServer = server
-                Rank = rank
-                RankMode = mode
-            }
-
-            return ()
-
-            // TODO: This should probably be the individual command's responsibility which then returns unit
-
-        | _ ->
-            return ()
-
-        // TODO: Rather than a big match, maybe each command can be separately defined and implemented, and this just
-        //       picks which to run. Maybe it should also handle the response?
-
-    | _ ->
-        return ()
-
+    // TODO: Figure out how the actions should then be called to run
     // TODO: Should this be enqueued instead so the function can return 202 immediately?
 }
